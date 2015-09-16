e65af2b7e1afbaa2b859bd0a2a73c1bf11559d80
Ilari Korhonen <ilari.korhonen@jyu.fi>
2015-09-16 18:56:15 +0300

    commented RodsMainWindow slots

--- src/rodsmainwindow.h
+++ src/rodsmainwindow.h
@@ -189,8 +189,10 @@ private slots:
     // qt slot which connects to allow overwrite checkbox toggled signal
     void on_allowOverwrite_toggled(bool checked);
 
+    // qt slot which connects to about action triggered signal
     void on_actionAbout_triggered();
 
+    // qt slot which connects to upload directory triggered signal
     void on_actionUploadDirectory_triggered();
 
 private:
