b086f2e7cbaea82e72aa70e415ce370af7e55f80
Ilari Korhonen <ilari.korhonen@jyu.fi>
2015-09-16 18:03:22 +0300

    functional RodsTransferWindow

--- src/rodstransferwindow.h
+++ src/rodstransferwindow.h
@@ -1,6 +1,21 @@
+/**
+ * @file rodstransferwindow.h
+ * @brief Definition of class RodsTransferWindow
+ *
+ * The RodsTransferWindow class extends the Qt widget base class QWidget
+ * and implements a transfer progress display window
+ *
+ * Copyright (C) 2014-2015 University of Jyväskylä. All rights reserved.
+ * License: The BSD 3-Clause License, see LICENSE file for details.
+ *
+ * @author Ilari Korhonen
+ */
+
 #ifndef RODSTRANSFERWINDOW_H
 #define RODSTRANSFERWINDOW_H
 
+// Qt framework headers
+#include <QVariant>
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QLabel>
@@ -13,12 +28,13 @@ class RodsTransferWindow : public QWidget
 
 public:
 
-    //
+    // Constructor initializes the transfer window object and its ui,
+    // sets window title as the provided title argument.
     explicit RodsTransferWindow(QString title);
 
 signals:
 
-    //
+    // Qt signal for signalling out the users request of cancelling.
     void cancelRequested();
 
 
@@ -30,11 +46,27 @@ public slots:
     //
     void updateMainProgress(QString currentMsg, int value);
 
+    //
+    void progressMarquee(QString currentMsg);
+
+    //
+    void invokeCancel();
+
 private:
 
+    // progress window main progress mar max
+    unsigned int progressMax;
+
+    // Qt UI layout instance
     QVBoxLayout *layout;
+
+    // Qt labels for displaying progress messages
     QLabel *mainProgressMsg, *subProgressMsg;
+
+    // Qt progress bar objects
     QProgressBar *mainProgressBar, *subProgressBar;
+
+    // Qt push button for cancel
     QPushButton *cancelButton;
 };
 
