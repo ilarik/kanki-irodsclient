d331b4a2a5fa20b088d2ff6b05c8af3e21258e10
Ilari Korhonen <ilarik@kth.se>
2017-11-14 13:11:16 -0700

    new dark color scheme, thank you @chexare for help with fine tuning!

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,30 @@ int main(int argc, char *argv[])
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
 
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(0x53,0x53,0x53));
+    darkPalette.setColor(QPalette::WindowText, QColor(0xd0, 0xd0, 0xd0));
+
+    darkPalette.setColor(QPalette::Base, QColor(0x40,0x40,0x40));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(0x45,0x45,0x45));
+
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+
+    darkPalette.setColor(QPalette::Text, QColor(0xe0, 0xe0, 0xe0));
+    darkPalette.setColor(QPalette::Button, QColor(0x40,0x40,0x40));
+    darkPalette.setColor(QPalette::ButtonText, QColor(0xe0,0xe0,0xe0));
+    darkPalette.setColor(QPalette::BrightText, Qt::white);
+
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(0x6f, 0x6e, 0x6e));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::white);
+
+    qApp->setPalette(darkPalette);
+    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+
     // create an iRODS Grid Browser window
     RodsMainWindow w;
 
