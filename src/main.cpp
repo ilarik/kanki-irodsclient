4a5ce7ea9c0397a949b159c76a32b8490e47b4e1
Ilari Korhonen <ilarik@kth.se>
2020-08-19 16:08:13 +0200

    cleanup main.cpp a bit

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  * @file main.cpp
  * @brief Kanki iRODS client main() function
  *
- * Copyright (C) 2016 KTH Royal Institute of Technology. All rights reserved.
+ * Copyright (C) 2016-2020 KTH Royal Institute of Technology. All rights reserved.
  * License: The BSD 3-Clause License, see LICENSE file for details.
  *
  * Copyright (C) 2014-2016 University of Jyväskylä. All rights reserved.
@@ -17,15 +17,17 @@
 int main(int argc, char *argv[])
 {
     // initialize Qt application
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
-#ifdef HIGH_DPI
     // enable retina-type screen High DPI support
-    a.setAttribute(Qt::AA_UseHighDpiPixmaps);
+#ifdef HIGH_DPI
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
 
-    qApp->setStyle(QStyleFactory::create("Fusion"));
+    // we force the GUI style
+    app.setStyle(QStyleFactory::create("Fusion"));
 
+    // we create a custom color palette
     QPalette darkPalette;
     darkPalette.setColor(QPalette::Window, QColor(0x53,0x53,0x53));
     darkPalette.setColor(QPalette::WindowText, QColor(0xd0, 0xd0, 0xd0));
@@ -45,15 +47,16 @@ int main(int argc, char *argv[])
     darkPalette.setColor(QPalette::Highlight, QColor(0x6f, 0x6e, 0x6e));
     darkPalette.setColor(QPalette::HighlightedText, Qt::white);
 
-    qApp->setPalette(darkPalette);
-    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+    app.setPalette(darkPalette);
+    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
-    // create an iRODS Grid Browser window
-    RodsMainWindow w;
+    // instantiate a main window bring it up
+    RodsMainWindow window;
+    window.show();
 
-    w.show();
-    w.doRodsConnect();
+    // invoke connect action
+    window.doRodsConnect();
 
-    // enter Qt main loop
-    return a.exec();
+    // enter Qt event loop
+    return (app.exec());
 }
